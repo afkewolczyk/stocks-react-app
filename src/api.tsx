d0163a4529c0789cd33b5bcfa7948357955aa4c9
Afke Holgersen <afke@responsive.ai>
2024-12-12 15:26:50 -0800

    Add new API routes

--- src/api.tsx
+++ src/api.tsx
@@ -1,6 +1,10 @@
 import axios from "axios";
-import { CompanyProfile, CompanySearch } from "./company";
-import { get } from "../node_modules/@jridgewell/set-array/src/set-array";
+import {
+  CompanyIncomeStatement,
+  CompanyKeyMetrics,
+  CompanyProfile,
+  CompanySearch,
+} from "./company";
 
 export interface SearchResponse {
   data: CompanySearch[];
@@ -27,6 +31,28 @@ export const getCompanyProfile = async (query: string) => {
   try {
     const data = await axios.get<CompanyProfile[]>(
       `https://financialmodelingprep.com/api/v3/profile/${query}?apikey=${process.env.REACT_APP_API_KEY}`
+    );
+    return data;
+  } catch (error: any) {
+    console.log("error message: ", error.message);
+  }
+};
+
+export const getKeyMetrics = async (query: string) => {
+  try {
+    const data = await axios.get<CompanyKeyMetrics[]>(
+      `https://financialmodelingprep.com/api/v3/key-metrics-ttm/${query}?limit=40&apikey=${process.env.REACT_APP_API_KEY}`
+    );
+    return data;
+  } catch (error: any) {
+    console.log("error message: ", error.message);
+  }
+};
+
+export const getIncomeStatement = async (query: string) => {
+  try {
+    const data = await axios.get<CompanyIncomeStatement[]>(
+      `https://financialmodelingprep.com/api/v3/income-statement/${query}?limit=40&apikey=${process.env.REACT_APP_API_KEY}`
     );
     return data;
   } catch (error: any) {
