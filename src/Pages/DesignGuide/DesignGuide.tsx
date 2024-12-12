9451b651d57d740a5fdf186d663f8d9a1325796f
Afke Holgersen <afke@responsive.ai>
2024-12-12 15:27:39 -0800

    Add table, income statement, company profile pages

--- src/Pages/DesignGuide/DesignGuide.tsx
+++ src/Pages/DesignGuide/DesignGuide.tsx
@@ -1,7 +1,23 @@
 import React from "react";
 import Table from "../../Components/Table/Table";
+import RatioList from "../../Components/RatioList/RatioList";
+import { CompanyKeyMetrics } from "../../company";
+import {
+  TestDataCompany,
+  testIncomeStatementData,
+} from "../../Components/Table/testData";
 
 interface Props {}
+
+const data = TestDataCompany;
+
+const tableConfig = [
+  {
+    label: "Market Cap",
+    render: (company: any) => company.marketCapTTM,
+    subTitle: "Total value of all a company's shares of stock",
+  },
+];
 
 const DesignGuide = (props: Props) => {
   return (
@@ -11,8 +27,8 @@ const DesignGuide = (props: Props) => {
         This is finsharks design page. This is where we will house various
         design aspects of the app.{" "}
       </h2>
-
-      <Table />
+      <RatioList data={data} config={tableConfig} />
+      <Table data={testIncomeStatementData} config={tableConfig} />
     </>
   );
 };
